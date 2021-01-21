c31b9903a26a073ab745db8132eea303a837cefb
newcodepusher <svyatoslav.d@blaize.tech>
2021-01-21 11:54:09 +0200

    add redux state and actions for crosschain

--- src/state/crosschain/hooks.ts
+++ src/state/crosschain/hooks.ts
@@ -1,9 +1,9 @@
 import { AppDispatch, AppState } from '../index'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { useActiveWeb3React } from '../../hooks'
-import useParsedQueryString from '../../hooks/useParsedQueryString'
+import { setCurrentTokenBalance } from './actions'
 
 export function useCrosschainState(): AppState['crosschain'] {
   return useSelector<AppState, AppState['crosschain']>(state => state.crosschain)
@@ -14,28 +14,10 @@ export function useDefaultsFromURLSearch():
   | undefined {
   const { chainId } = useActiveWeb3React()
   const dispatch = useDispatch<AppDispatch>()
-  const parsedQs = useParsedQueryString()
-  const [result, setResult] = useState<
-    { inputCurrencyId: string | undefined; outputCurrencyId: string | undefined } | undefined
-    >()
 
   useEffect(() => {
-    if (!chainId) return
-    const parsed = queryParametersToCrosschainState(parsedQs)
-
-    dispatch(
-      replaceCrosschainState({
-        typedValue: parsed.typedValue,
-        field: parsed.independentField,
-        inputCurrencyId: parsed[Field.INPUT].currencyId,
-        outputCurrencyId: parsed[Field.OUTPUT].currencyId || '0xF0939011a9bb95c3B791f0cb546377Ed2693a574',
-        recipient: parsed.recipient
-      })
-    )
-
-    setResult({ inputCurrencyId: parsed[Field.INPUT].currencyId, outputCurrencyId: parsed[Field.OUTPUT].currencyId })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    dispatch(setCurrentTokenBalance({balance:'3543'}))
   }, [dispatch, chainId])
 
-  return result
+  return undefined
 }
