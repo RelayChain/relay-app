f50eedecd96a5a36e78a56637e1cc39cd98ae3f7
microcrontop <microcrontop@gmail.com>
2021-01-28 02:04:13 +0200

    add mainnet avalanche

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -84,6 +84,9 @@ export function useENSRegistrarContract(withSignerIfPossible?: boolean): Contrac
       case ChainId.FUJI:
         address = '0x59F49F35495854023983C877A7781eAb3A63A0f2'
         break
+      case ChainId.AVALANCHE:
+        address = '0xd2b4cD0966D08fE000CED7B73DFcbD2Ec000759F'
+        break
     }
   }
   return useContract(address, ENS_ABI, withSignerIfPossible)
