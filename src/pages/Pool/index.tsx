6f1bfa492ade00b0427ca5fda5728d6f96d9e8ea
Secret Elf <iamsecretsanta124@protonmail.com>
2021-02-02 11:33:06 -0600

    remove link to uniswap on avax

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -186,14 +186,16 @@ export default function Pool() {
               </EmptyProposals>
             ) : allV2PairsWithLiquidity?.length > 0 || stakingPairs?.length > 0 ? (
               <>
-                <ButtonSecondary>
-                  <RowBetween>
-                    <ExternalLink href={'https://uniswap.info/account/' + account}>
-                      Account analytics and accrued fees
-                    </ExternalLink>
-                    <span> ↗</span>
-                  </RowBetween>
-                </ButtonSecondary>
+                { chainId && chainId === ChainId.MAINNET && 
+                  <ButtonSecondary>
+                    <RowBetween>
+                      <ExternalLink href={'https://uniswap.info/account/' + account}>
+                        Account analytics and accrued fees
+                      </ExternalLink>
+                      <span> ↗</span>
+                    </RowBetween>
+                  </ButtonSecondary>
+                }
                 {v2PairsWithoutStakedAmount.map(v2Pair => (
                   <FullPositionCard key={v2Pair.liquidityToken.address} pair={v2Pair} />
                 ))}
