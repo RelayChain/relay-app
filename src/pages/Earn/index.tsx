fba26c8391f149411644eccf7784fbd127011d6f
Secret Elf <iamsecretsanta124@protonmail.com>
2021-02-02 18:15:51 -0600

    close pools until tomorrow

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -121,7 +121,8 @@ export default function Earn() {
 
   return (
     <PageWrapper gap="lg" justify="center">
-      <VoteCard>
+      <h2>Pools closed. Come back tomorrow degens :)</h2>
+      <VoteCard style={{ opacity: '.5', pointerEvents: 'none'}}>
         <CardBGImage />
         <CardNoise />
         <CardSection>
@@ -158,7 +159,7 @@ export default function Earn() {
         <CardNoise />
       </VoteCard>
 
-      <RowBetween>
+      <RowBetween style={{ opacity: '.5', pointerEvents: 'none'}}>
         <ResponsiveButtonSecondary as={Link} padding="6px 8px" to={`create/${ chainId === ChainId.MAINNET ? 'ETH' : 'AVAX' }`} style={{ margin: '5px 5px 5px auto' }}>
           Create New Pool Pair
         </ResponsiveButtonSecondary>
