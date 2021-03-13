1d184f5460186585816ea08d0e3352e18f8da3e5
winter256 <haroldbudd64@gmail.com>
2021-03-13 03:49:04 +0200

    Fix token order in pair zero bnb

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -108,11 +108,10 @@ export default function Earn() {
     [ChainId.SMART_CHAIN]: [
       // https://bscscan.com/tx/0x11e8e1766788581dd7597c5eae53a4ebb0709f45b22850822f54c553e9eaca8c
       {
-        baseSymbol: 'BNB',
-        // baseAddress: 'BNB',
-        baseAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-        otherSymbol: 'ZERO',
-        otherAddress: '0x1f534d2B1ee2933f1fdF8e4b63A44b2249d77EAf',
+        baseSymbol: 'ZERO',
+        baseAddress: '0x1f534d2B1ee2933f1fdF8e4b63A44b2249d77EAf',
+        otherSymbol: 'BNB',
+        otherAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
       },
       // {
       //   baseSymbol: 'BNB',
