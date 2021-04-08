9dc9898411551ef2034801910ec3cb70213551d1
iamdavidk <liugrisha@gmail.com>
2021-04-09 01:55:48 +0800

    CurrencyInput component BG update

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -90,7 +90,7 @@ const InputPanel = styled.div<{ hideInput?: boolean }>`
   ${({ theme }) => theme.flexColumnNoWrap}
   position: relative;
   border-radius: ${({ hideInput }) => (hideInput ? '8px' : '20px')};
-  background-color: ${({ theme }) => theme.bg2};
+  background-color: ${({ theme }) => theme.bg1};
   z-index: 1;
 `
 
