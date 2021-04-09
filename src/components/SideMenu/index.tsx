2043a51e0c3a0ded1fa9d2443eab71c94f85c19f
Aleksandr Kravets <a.kravets@alterego.digital>
2021-04-09 14:08:14 +0300

    Fixed colors and icons for SideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
-import { BarChart, Book, CreditCard, DollarSign, RefreshCw, Home } from 'react-feather'
 import { useTranslation } from 'react-i18next'
 import { ExternalLink } from '../../theme'
 import useWindowDimensions from '../../hooks/useWindowDimensions'
+import Icon from '../Icon'
+import { useHistory, useLocation } from 'react-router-dom'
 
 const SideMenuWrapper = styled.div<{ open?: boolean }>`
   height: 100%;
@@ -13,7 +14,6 @@ const SideMenuWrapper = styled.div<{ open?: boolean }>`
   position: fixed;
   left: 0;
   top: 0;
-  background-color: rgba(0, 0, 0, 0.25);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -28,8 +28,9 @@ const HeaderLinks = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding-left: 35px;
+  padding-left: 45px;
   height: 34%;
+  margin-bottom: 10px;
   font-weight: 600;
   ${({ theme }) => theme.mediaWidth.upToMedium`
   padding-left: 0px;
@@ -41,43 +42,31 @@ const StyledNavLink = styled(NavLink).attrs({
 })`
   ${({ theme }) => theme.flexRowNoWrap}
   align-items: left;
-  border-radius: 3rem;
   outline: none;
   cursor: pointer;
+  text-transform: uppercase;
+  font-size: 13px;
   text-decoration: none;
   color: ${({ theme }) => theme.text2};
-  font-size: 1rem;
   width: fit-content;
-  margin: 0 16px;
   font-weight: 600;
   transition: all 0.2s ease-in-out;
   font-family: 'Poppins', sans-serif;
   &.${activeClassName} {
-    border-radius: 12px;
-    font-weight: 600;
     color: ${({ theme }) => theme.white};
   }
-
   :hover,
   :focus {
     color: ${({ theme }) => theme.white};
   }
-  ${({ theme }) => theme.mediaWidth.upToMedium`
-      font-size: .85rem;
-      margin: 0 6px;
-  `};
 `
-
 const HeaderExternalLink = styled(ExternalLink)`
-  margin: 0 16px;
-  color: #c3c5cb;
+  color: ${({ theme }) => theme.text2};
   transition: all 0.2s ease-in-out;
+  text-transform: uppercase;
+  font-size: 13px;
   font-family: 'Poppins', sans-serif;
   font-weight: 600;
-  ${({ theme }) => theme.mediaWidth.upToMedium`
-    margin: 0 6px;
-    font-size: .85rem;
-  `};
   :hover,
   :focus {
     color: ${({ theme }) => theme.white};
@@ -85,6 +74,10 @@ const HeaderExternalLink = styled(ExternalLink)`
   }
 `
 
+const IconLink = styled.span`
+  display: inline-block;
+  margin-right: 20px;
+`
 export interface SideMenuProps {
   open: boolean
   setOpen: () => void
@@ -93,6 +86,15 @@ export interface SideMenuProps {
 export default function SideMenu({ open, setOpen }: SideMenuProps) {
   const { t } = useTranslation()
   const { width } = useWindowDimensions()
+  const history = useHistory()
+  const location = useLocation()
+
+  const [pathname, setPathname] = useState(location.pathname)
+  history.listen(location => {
+    console.log(location.pathname)
+    setPathname(location.pathname)
+  })
+
 
   const hanldeSidemenuOpen = () => {
     if (width < 961) {
@@ -104,32 +106,44 @@ export default function SideMenu({ open, setOpen }: SideMenuProps) {
     <SideMenuWrapper>
       <HeaderLinks>
         <StyledNavLink id={`swap-nav-link`} to={'/home'} onClick={hanldeSidemenuOpen}>
-          <Home size={16} style={{ marginRight: '20px', marginTop: '2px' }} />
-          {t('home')}
+          <IconLink>
+            <Icon icon="home"  active={pathname == '/home'}/>
+          </IconLink>
+          {t('Home')}
         </StyledNavLink>
         <StyledNavLink id={`swap-nav-link`} to={'/swap'} onClick={hanldeSidemenuOpen}>
-          <RefreshCw size={16} style={{ marginRight: '20px', marginTop: '2px' }} />
-          {t('swap')}
+          <IconLink>
+            <Icon icon="swap"  active={pathname == '/swap'} />
+          </IconLink>
+          {t('Swap')}
         </StyledNavLink>
         <StyledNavLink id={`earn-nav-link`} to={'/earn'} onClick={hanldeSidemenuOpen}>
-          <DollarSign size={16} style={{ marginRight: '20px', marginTop: '2px' }} />
+          <IconLink>
+            <Icon icon="earn" active={pathname == '/earn'} />
+          </IconLink>
           {t('Earn')}
         </StyledNavLink>
         <span onClick={hanldeSidemenuOpen}>
           <HeaderExternalLink href={`https://buy.0.exchange`}>
-            <CreditCard size={16} style={{ marginRight: '20px', marginTop: '2px', marginBottom: '-3px' }} />
+            <IconLink>
+              <Icon icon="market"/>
+            </IconLink>
             Buy
           </HeaderExternalLink>
         </span>
         <span onClick={hanldeSidemenuOpen}>
           <HeaderExternalLink href={`https://charts.0.exchange`}>
-            <BarChart size={16} style={{ marginRight: '20px', marginTop: '2px', marginBottom: '-3px' }} />
+            <IconLink>
+              <Icon icon="charts"/>
+            </IconLink>
             Charts
           </HeaderExternalLink>
         </span>
         <span onClick={hanldeSidemenuOpen}>
           <HeaderExternalLink href={`https://zero-exchange.gitbook.io/zero-exchange-docs/`}>
-            <Book size={16} style={{ marginRight: '20px', marginTop: '2px', marginBottom: '-2px' }} />
+            <IconLink>
+              <Icon icon="planet"/>
+            </IconLink>
             Guides
           </HeaderExternalLink>
         </span>
