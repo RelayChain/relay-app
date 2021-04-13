240424ca8707798236cdc40e2751ccd80f26b610
Aleksandr Kravets <a.kravets@alterego.digital>
2021-04-13 12:58:07 +0300

    Second part of home_page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -114,58 +114,60 @@ export default function App() {
   const [isDark] = useDarkModeManager()
   return (
     <Suspense fallback={null}>
-      <Route component={DarkModeQueryParamReader} />
-      <Title href="/">
-        <img width={'100%'} src={isDark ? LogoDark : Logo} alt="logo" />
-      </Title>
+      <GraphQLProvider>
+        <Route component={DarkModeQueryParamReader} />
+        <Title href="/">
+          <img width={'100%'} src={isDark ? LogoDark : Logo} alt="logo" />
+        </Title>
 
-      <AppWrapper>
-        <MenuBurger open={open} setOpen={() => setOpen(!open)} />
-        <SideMenu open={open} setOpen={() => setOpen(!open)} />
-        <div className="snow-bg"></div>
-        <div className="bg-darken"></div>
-        <URLWarning />
+        <AppWrapper>
+          <MenuBurger open={open} setOpen={() => setOpen(!open)} />
+          <SideMenu open={open} setOpen={() => setOpen(!open)} />
+          <div className="snow-bg"></div>
+          <div className="bg-darken"></div>
+          <URLWarning />
 
-        <BodyWrapper>
-          <HeaderWrapper>
-            <Header />
-          </HeaderWrapper>
-          <Popups />
-          <Polling />
-          <TopLevelModals />
-          <Web3ReactManager>
-            <Switch>
-              <Route exact strict path="/home" component={Home} />
-              <Route exact strict path="/swap" component={Swap} />
-              <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
-              <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
-              <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
-              <Route exact strict path="/find" component={PoolFinder} />
-              {/*<Route exact strict path="/pool" component={Pool} />*/}
-              <Route exact strict path="/pools" component={Pools} />
-              <Route exact strict path="/guides" component={Guides} />
-              <Route exact strict path="/vote" component={Vote} />
-              <Route exact strict path="/create" component={RedirectToAddLiquidity} />
-              <Route exact path="/add" component={AddLiquidity} />
-              <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-              <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-              <Route exact path="/create" component={AddLiquidity} />
-              <Route exact path="/create/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-              <Route exact path="/create/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-              <Route exact strict path="/remove/v1/:address" component={RemoveV1Exchange} />
-              <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
-              <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-              <Route exact strict path="/migrate/v1" component={MigrateV1} />
-              <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange} />
-              <Route exact strict path="/zero/:currencyIdA/:currencyIdB" component={Manage} />
-              <Route exact strict path="/vote/:id" component={VotePage} />
-              <Route exact strict path="/transfer" component={Transfer} />
-              <Route component={RedirectPathToHomeOnly} />
-            </Switch>
-          </Web3ReactManager>
-          <Marginer />
-        </BodyWrapper>
-      </AppWrapper>
+          <BodyWrapper>
+            <HeaderWrapper>
+              <Header />
+            </HeaderWrapper>
+            <Popups />
+            <Polling />
+            <TopLevelModals />
+            <Web3ReactManager>
+              <Switch>
+                <Route exact strict path="/home" component={Home} />
+                <Route exact strict path="/swap" component={Swap} />
+                <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
+                <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
+                <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
+                <Route exact strict path="/find" component={PoolFinder} />
+                {/*<Route exact strict path="/pool" component={Pool} />*/}
+                <Route exact strict path="/pools" component={Pools} />
+                <Route exact strict path="/guides" component={Guides} />
+                <Route exact strict path="/vote" component={Vote} />
+                <Route exact strict path="/create" component={RedirectToAddLiquidity} />
+                <Route exact path="/add" component={AddLiquidity} />
+                <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                <Route exact path="/create" component={AddLiquidity} />
+                <Route exact path="/create/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                <Route exact path="/create/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                <Route exact strict path="/remove/v1/:address" component={RemoveV1Exchange} />
+                <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
+                <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
+                <Route exact strict path="/migrate/v1" component={MigrateV1} />
+                <Route exact strict path="/migrate/v1/:address" component={MigrateV1Exchange} />
+                <Route exact strict path="/zero/:currencyIdA/:currencyIdB" component={Manage} />
+                <Route exact strict path="/vote/:id" component={VotePage} />
+                <Route exact strict path="/transfer" component={Transfer} />
+                <Route component={RedirectPathToHomeOnly} />
+              </Switch>
+            </Web3ReactManager>
+            <Marginer />
+          </BodyWrapper>
+        </AppWrapper>
+      </GraphQLProvider>
     </Suspense>
   )
 }
