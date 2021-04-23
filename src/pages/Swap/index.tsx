8c396917beb85c02e4a37222887ddf5a7f58a521
mrbot22 <mrbot22@protonmail.com>
2021-04-23 20:04:50 +0300

    fix harcoded icon

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -487,7 +487,6 @@ export default function Swap() {
       showConfirmTransferModal()
     }
   }
-
   return (
     <>
       <TokenWarningModal
@@ -518,7 +517,7 @@ export default function Swap() {
             isOpen={confirmTransferModalOpen}
             onDismiss={hideConfirmTransferModal}
             // @ts-ignore
-            transferTo={transferTo}
+            transferTo={isCrossChain ? targetChain : transferTo}
             activeChain={chainId ? CHAIN_LABELS[chainId] : 'Ethereum'}
             changeTransferState={onChangeTransferState}
             tokenTransferState={crosschainTransferStatus}
