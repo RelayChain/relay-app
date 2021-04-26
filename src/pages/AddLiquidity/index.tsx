7a3487d4b5356997b6343881081d1638fdd7169a
winter256 <haroldbudd64@gmail.com>
2021-04-26 16:23:55 +0300

    Fix adding liquidity eth

--- src/pages/AddLiquidity/index.tsx
+++ src/pages/AddLiquidity/index.tsx
@@ -165,21 +165,15 @@ export default function AddLiquidity({
       [Field.CURRENCY_B]: calculateSlippageAmount(parsedAmountB, noLiquidity ? 0 : allowedSlippage)[0]
     }
 
-    let estimate,
-      method: (...args: any) => Promise<TransactionResponse>,
-      args: Array<string | string[] | number>,
-      value: BigNumber | null
-    if (
-      currencyA === ETHER ||
-      currencyB === ETHER ||
-      currencyB === BNB ||
-      currencyA === AVAX ||
-      currencyB === AVAX ||
-      currencyB === BNB ||
-      currencyA === WBNB ||
-      currencyB === WBNB 
-    ) {
-      const tokenBIsETH = currencyB === ETHER || currencyB === AVAX || currencyB === BNB
+    const ALL_ETHERS = [ETHER, BNB, AVAX];
+
+    let estimate;
+    let method: (...args: any) => Promise<TransactionResponse>;
+    let args: Array<string | string[] | number>;
+    let value: BigNumber | null;
+
+    if ([currencyA, currencyB].some(c => ALL_ETHERS.includes(c))) {
+      const tokenBIsETH = ALL_ETHERS.includes(currencyB);
       estimate = router.estimateGas.addLiquidityETH
       method = router.addLiquidityETH
       args = [
