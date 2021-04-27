41546ec1a3801f79d01f637d5c31b6188aad80b4
Aleksandr Kravets <a.kravets@alterego.digital>
2021-04-27 19:02:52 +0300

    Changed styles for toogle pool

--- src/components/TooglePool/index.tsx
+++ src/components/TooglePool/index.tsx
@@ -4,24 +4,24 @@ import styled from 'styled-components'
 
 const ToggleElement = styled.span<{ isActive?: boolean; isOnSwitch?: boolean }>`
   display: flex;
-  height: 100%;
+  justify-content: center;
   align-items: center;
-  padding: 0.25rem 0.5rem;
-  background: ${({ theme, isActive, isOnSwitch }) => (isActive ? (isOnSwitch ? theme.primary1 : theme.text4) : 'none')};
-  color: ${({ theme, isActive, isOnSwitch }) => (isActive ? (isOnSwitch ? theme.white : theme.text2) : theme.text3)};
-  font-size: 1rem;
-  font-weight: 400;
-
+  width: 80px;
+  height: 100%;
+  font-size: 13px;
   padding: 0.35rem 0.6rem;
   border-radius: 12px;
   background: ${({ isActive, isOnSwitch }) => (isActive ? (isOnSwitch ? '#6752F7' : '#6752F7') : 'none')};
   color: ${({ theme, isActive, isOnSwitch }) => (isActive ? (isOnSwitch ? theme.white : theme.white) : theme.text2)};
-  font-size: 1rem;
   font-weight: ${({ isOnSwitch }) => (isOnSwitch ? '500' : '400')};
   :hover {
     user-select: ${({ isOnSwitch }) => (isOnSwitch ? 'none' : 'initial')};
-    color: ${({ theme, isActive, isOnSwitch }) => (isActive ? (isOnSwitch ? theme.white : theme.white) : theme.text3)};
+    color: ${({ theme }) => theme.white};
   }
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+  width: 60px;
+  font-size: 12px;
+  `};
 `
 
 const StyledToggle = styled.button<{ isActive?: boolean; activeElement?: boolean }>`
@@ -32,20 +32,84 @@ const StyledToggle = styled.button<{ isActive?: boolean; activeElement?: boolean
   box-shadow: inset 2px 2px 5px rgb(255 255 255 / 10%);
   -webkit-backdrop-filter: blur(28px);
   backdrop-filter: blur(28px);
-  border-radius: 44px;
   display: flex;
   width: fit-content;
   cursor: pointer;
   outline: none;
   padding: 0;
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+  height: 36px;
+  `};
 `
 
-const StackedWrap = styled.div`
+const StyledStacked = styled.div`
+  display: inline-block;
   margin-right: 10px;
-  cursor: pointer;
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-  font-size: 14px;
-`};
+  > input {
+    opacity: 0;
+  }
+  > input + label {
+    position: relative;
+    padding-left: 25px;
+    cursor: pointer;
+    ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    font-szie: 13px;
+    `};
+    &:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 1px;
+      width: 17px;
+      height: 17px;
+      border: 1px solid rgba(47,53,115,0.92);
+      border-radius: 10px;
+      box-shadow: inset 0 1px 3px rgb(0 0 0 / 30%);
+      background: rgba(47,53,115,0.12);
+    }
+    &:after {
+      content: '✔';
+      position: absolute;
+      top: -6px;
+      left: 2px;
+      font-size: 21px;
+      color: #6752F7;
+      transition: all 0.2s;
+    }
+  }
+  > input:not(:checked) + label {
+    &:after {
+      opacity: 0;
+      transform: scale(0);
+    }
+  }
+  > input:disabled:not(:checked) + label {
+    &:before {
+      box-shadow: none;
+      border-color: #bbb;
+      background-color: #ddd;
+    }
+  }
+  > input:checked + label {
+    &:after {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  > input:disabled:checked + label {
+    &:after {
+      color: #999;
+    }
+  }
+  > input:disabled + label {
+    color: #aaa;
+  }
+  > input:checked:focus + label,
+  input:not(:checked):focus + label {
+    &:before {
+      border: 1px dotted blue;
+    }
+  }
 `
 
 export interface ToggleProps {
@@ -55,13 +119,13 @@ export interface ToggleProps {
   setShowStaked: () => void
 }
 
-export default function Toggle({ isActive, toggle, isStaked, setShowStaked}: ToggleProps) {
+export default function Toggle({ isActive, toggle, isStaked, setShowStaked }: ToggleProps) {
   return (
-      <Row>
-        <StackedWrap>
-        <input type="checkbox" id="stacked" checked={isStaked} onChange={setShowStaked}/>
-        <label htmlFor="stacked">Staked only</label>
-        </StackedWrap>
+    <Row>
+        <StyledStacked onClick={setShowStaked}>
+          <input type="checkbox" checked={isStaked} />
+          <label>Staked only</label>
+        </StyledStacked>
       <StyledToggle isActive={isActive} onClick={toggle}>
         <ToggleElement isActive={isActive} isOnSwitch={true}>
           Live
@@ -70,6 +134,6 @@ export default function Toggle({ isActive, toggle, isStaked, setShowStaked}: Tog
           Finished
         </ToggleElement>
       </StyledToggle>
-      </Row>
+    </Row>
   )
 }
