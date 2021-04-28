beb9dca7161c6a2a7fea14a9cb20fe7996b7586f
Anomous-dev-v <valeriia.danyliuk@inventorsoft.co>
2021-04-28 18:47:35 +0300

    transactions table on homepage

--- src/components/TransactionLine/index.tsx
+++ src/components/TransactionLine/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
-
-import { TransactionTypes } from './../../graphql/types'
 import styled from 'styled-components'
+
+import DoubleCurrencyLogo from '../DoubleLogo'
+import { TransactionTypes } from './../../graphql/types'
 import toEllipsis from './../../utils/toEllipsis'
 import useWindowDimensions from './../../hooks/useWindowDimensions'
 
-const Flex = styled.div`
-  display: flex;
-  flex-direction: column;
+const Logo = styled(DoubleCurrencyLogo)`
+  margin-bottom: 20px;
 `
+const Flex = styled.span`
+  display: flex;
+`
+
 const Text = styled.div`
   font-family: Poppins;
   font-weight: 500;
@@ -17,39 +21,44 @@ const Text = styled.div`
   letter-spacing: -0.01em;
   color: #b7c0f9;
   opacity: 0.8;
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-  font-size: 13px;
+  text-align: center
+  margin-top:5px
+    ${({ theme }) => theme.mediaWidth.upToSmall`
+  font-size: 14px;
   border-radius: 28px;
-  white-space: nowrap;
 `};
 `
 const TextTransition = styled(Text)`
-  color: #B368FC;
+  color: #b368fc;
 `
 const TextValue = styled(Text)`
   ${({ theme }) => theme.mediaWidth.upToSmall`
-text-align: center
+text-align: right
+margin-right:25px;
 `};
 `
 const TextColumnLeft = styled(Text)`
   font-size: 12px;
   opacity: 0.4;
   margin-top: 30px;
+  text-align: left;
 `
 const TextColumnRight = styled(TextColumnLeft)`
   opacity: 1;
-  text-align: center;
+  text-align: right;
+  margin-right: 25px;
   position: relative;
 `
 const DropDownBoxArrow = styled.div`
   position: absolute;
   cursor: pointer;
   top: 50%;
-  right: 25px;
+  right:0
+  margin-right: 5px;
   transform: translateY(-50%);
 `
 const DropDownBoxArrowUp = styled(DropDownBoxArrow)`
-  top: 33px;
+  top: 25px;
   transform: translateY(0);
 `
 const InnerWrap = styled.div`
@@ -59,18 +68,22 @@ const InnerWrap = styled.div`
   right: 0;
   bottom: 0;
 `
+
 export type TransactionLineProps = TransactionTypes & {}
 
 const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
   const { width } = useWindowDimensions()
   const [isOpen, setisOpen] = useState<boolean>(false)
+
   if (mints.length < 1 && burns.length < 1 && swaps.length < 1) return null
 
   const getData = () => {
     switch (true) {
       case mints.length > 0:
         return {
-          type: `Add ${mints[0].pair.token0.symbol} and ${mints[0].pair.token1.symbol}`,
+          token0: mints[0].pair.token0,
+          token1: mints[0].pair.token1,
+          type: `Add ${mints[0].pair.token0.symbol} / ${mints[0].pair.token1.symbol}`,
           totalValue: mints[0].amountUSD,
           amount0: mints[0].amount0,
           amount1: mints[0].amount1,
@@ -80,7 +93,9 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
 
       case burns.length > 0:
         return {
-          type: `Remove ${burns[0].pair.token0.symbol} and ${burns[0].pair.token1.symbol}`,
+          token0: burns[0].pair.token0,
+          token1: burns[0].pair.token1,
+          type: `Remove ${burns[0].pair.token0.symbol} / ${burns[0].pair.token1.symbol}`,
           totalValue: burns[0].amountUSD,
           amount0: burns[0].amount0,
           amount1: burns[0].amount1,
@@ -90,7 +105,9 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
 
       case swaps.length > 0:
         return {
-          type: `Swap ${swaps[0].pair.token0.symbol} and ${swaps[0].pair.token1.symbol}`,
+          token0: swaps[0].pair.token0,
+          token1: swaps[0].pair.token1,
+          type: `Swap ${swaps[0].pair.token0.symbol} / ${swaps[0].pair.token1.symbol}`,
           totalValue: swaps[0].amountUSD,
           amount0: swaps[0].amount0Out,
           amount1: swaps[0].amount1Out,
@@ -107,12 +124,16 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
 
   if (!data.transaction) return null
 
-  const { type, totalValue, amount0, amount1, transaction, date } = data
+  const { type, totalValue, amount0, amount1, transaction, date, token0, token1 } = data
+
   return (
     <>
-      <Flex>
-        <Text>{type}</Text>
-        {width < 500 && isOpen && (
+      <div>
+        <Flex>
+          <Logo currency0={token0} currency1={token1} size={24} style={{ marginRight: '8px' }} />
+          <Text style={{ marginLeft: '10px' }}>{type}</Text>
+        </Flex>
+        {width < 720 && isOpen && (
           <>
             <TextColumnLeft>Amount #1</TextColumnLeft>
             <TextColumnLeft>Amount #2</TextColumnLeft>
@@ -120,24 +141,13 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
             <TextColumnLeft>Date</TextColumnLeft>
           </>
         )}
-      </Flex>
-      <Flex>
-        <TextValue>
-          {width > 1424
-            ? Number(totalValue).toFixed(8)
-            : width > 1000
-            ? Number(totalValue).toFixed(4)
-            : Number(totalValue).toFixed(2)}
-        </TextValue>
-        {width < 500 && isOpen && (
+      </div>
+      <div>
+        <TextValue>${Number(totalValue).toFixed(2)}</TextValue>
+
+        {width < 720 && isOpen && (
           <>
-            <TextColumnRight>
-              {width > 1424
-                ? Number(totalValue).toFixed(8)
-                : width > 1000
-                ? Number(totalValue).toFixed(4)
-                : Number(totalValue).toFixed(2)}
-            </TextColumnRight>
+            <TextColumnRight>${Number(totalValue).toFixed(2)}</TextColumnRight>
             <TextColumnRight>{Number(amount1).toFixed(6)}</TextColumnRight>
             <TextColumnRight style={{ color: '#1CB0F9', position: 'relative' }}>
               {width > 1424
@@ -159,8 +169,9 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
             </TextColumnRight>
           </>
         )}
-      </Flex>
-      {width < 500 && !isOpen && (
+      </div>
+
+      {width < 720 && !isOpen && (
         <DropDownBoxArrow>
           <svg width="22" height="12" viewBox="0 0 22 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -173,7 +184,8 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
           </svg>
         </DropDownBoxArrow>
       )}
-      {width < 500 && isOpen && (
+
+      {width < 720 && isOpen && (
         <DropDownBoxArrowUp>
           <svg width="22" height="12" viewBox="0 0 22 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -186,7 +198,8 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
           </svg>
         </DropDownBoxArrowUp>
       )}
-      {width > 500 && (
+
+      {width > 720 && (
         <>
           <Text>
             {width > 1424
@@ -198,7 +211,7 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
           <Text>{Number(amount1).toFixed(6)}</Text>
           <TextTransition>
             {width > 1424
-              ? toEllipsis(transaction, 20)
+              ? toEllipsis(transaction, 24)
               : width > 1000
               ? toEllipsis(transaction, 28)
               : toEllipsis(transaction, 30)}
@@ -216,7 +229,8 @@ const TransactionLine = ({ mints, burns, swaps }: TransactionLineProps) => {
           </Text>
         </>
       )}
-      {width < 500 && <InnerWrap onClick={() => setisOpen(!isOpen)} />}
+
+      {width < 720 && <InnerWrap onClick={() => setisOpen(!isOpen)} />}
     </>
   )
 }
