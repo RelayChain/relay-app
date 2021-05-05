825599e06753af03cdab88bb04578a8c65784e81
Secret Elf <iamsecretsanta124@protonmail.com>
2021-05-05 13:16:45 -0500

    date updates

--- src/constants/idos.ts
+++ src/constants/idos.ts
@@ -6,14 +6,14 @@ export const IDO_LIST = [
     logo: '/images/idos/wasder-logo.png',
     tierName: '-', // if no tier, leave blank
     tierLogo: null, // if you include this image, it will override the tierName
-    launchDate: new Date('May 07, 2021 06:00:00'),
-    endDate: new Date('May 8, 2021 06:00:00'),
+    launchDate: new Date('May 07, 2021 14:00:00'),
+    endDate: new Date('May 8, 2021 14:00:00'),
     totalRaise: '$150,000',
     allocationMin: 'TBD',
     allocationMax: 'TBD',
     allocationCurrency: 'USDT',
     description: 'Wasder - Find friends, join communities, and share content. Wasder brings old and new friends together around your passion for gaming. The Wasder IDO token sale will be May 7, 2021 on 0.exchange. If you received a whitelist allocation, please KYC above to be approved for the IDO.',
-    distributionDate: new Date('May 8, 2021 06:00:00'),
+    distributionDate: new Date('May 7, 2021 14:00:00'),
     allocationWinningAmount: 'TBD',
     socials: [
       {
