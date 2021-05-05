46816f4c0855691b89b217da6be15f122b056197
Secret Elf <iamsecretsanta124@protonmail.com>
2021-05-05 14:17:45 -0500

    amount change

--- src/constants/idos.ts
+++ src/constants/idos.ts
@@ -14,7 +14,7 @@ export const IDO_LIST = [
     allocationCurrency: 'USDT',
     description: 'Wasder - Find friends, join communities, and share content. Wasder brings old and new friends together around your passion for gaming. The Wasder IDO token sale will be May 7, 2021 on 0.exchange. If you received a whitelist allocation, please KYC above to be approved for the IDO.',
     distributionDate: new Date('May 7, 2021 14:00:00'),
-    allocationWinningAmount: 'TBD',
+    allocationWinningAmount: '$150',
     socials: [
       {
         type: 'TELEGRAM',
