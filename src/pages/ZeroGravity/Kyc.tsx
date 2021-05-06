81531f9442cb6cc331e4fca980466fdfb09c0c8f
Secret Elf <iamsecretsanta124@protonmail.com>
2021-05-06 11:53:20 -0500

    fix placeholder

--- src/pages/ZeroGravity/Kyc.tsx
+++ src/pages/ZeroGravity/Kyc.tsx
@@ -135,7 +135,7 @@ export default function ZeroGravityKyc() {
                 <input
                   className="input"
                   type="text"
-                  placeholder="Name"
+                  placeholder="Email"
                   onChange={handleEmailState}
                   value={emailState}
                 />
