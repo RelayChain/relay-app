363f6b86c96c295aa1e91a893bfe6df46a8ed0a9
Secret Elf <iamsecretsanta124@protonmail.com>
2021-12-05 19:34:45 -0600

    fix logo

--- src/components/BlockchainLogo/index.tsx
+++ src/components/BlockchainLogo/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 import { getCurrencyLogoImage } from 'components/CurrencyLogo'
 import { logoByName } from './logos'
+import styled from 'styled-components'
 
 const StyledEthereumLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
-  border-radius: 24px;
+  border-radius: 100%;
   margin-left: 5px;
   margin-right: 5px;
 `
